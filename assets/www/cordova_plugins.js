bce9f107e936cd7b901f93969429176d7208b0fb
Fernando <fernando@sisdado.com>
2015-03-18 11:04:21 -0300

    teste51

--- assets/www/cordova_plugins.js
+++ assets/www/cordova_plugins.js
@@ -6,12 +6,21 @@ module.exports = [
         "clobbers": [
             "SQLitePlugin"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.brodysoft.sqlitePlugin": "1.0.5"
+    "com.brodysoft.sqlitePlugin": "1.0.5",
+    "org.apache.cordova.console": "0.2.13",
+    "org.apache.cordova.device": "0.3.0"
 }
 // BOTTOM OF METADATA
 });
