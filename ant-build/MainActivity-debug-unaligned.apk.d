65f4c2159cb68ceae526069eaab7fdfaabfbdedf
Fernando <fernando@sisdado.com>
2015-03-18 11:37:26 -0300

    Revert "teste51"
    This reverts commit 6b13fb96ee4ba1f3f64ee4e140197251b230fda6.

---
+++ ant-build/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\xampp\htdocs\NovoApp\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+C:\xampp\htdocs\NovoApp\platforms\android\ant-build\MainActivity.ap_ \
+C:\xampp\htdocs\NovoApp\platforms\android\ant-build\classes.dex \
+C:\xampp\htdocs\NovoApp\platforms\android\CordovaLib\ant-build\classes.jar \
